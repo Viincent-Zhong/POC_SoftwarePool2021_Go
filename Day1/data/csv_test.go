c11f16a8ab6f3bc120f5ed49512c67e853500607
Vincent Zhong <vzhong@localhost.localdomain>
2021-02-15 17:48:32 +0100

    [ADD] Test given

--- Day1/data/csv_test.go
+++ Day1/data/csv_test.go
@@ -3,5 +3,41 @@ package data
 import "testing"
 
 func TestLineToCSV(t *testing.T) {
+	tests := []struct {
+		in  string
+		out []string
+		err bool
+	}{
+		{
+			in:  "jean,34,france",
+			out: []string{"jean", "34", "france"},
+			err: false,
+		},
+		{
+			in:  "j,j,j,j",
+			out: nil,
+			err: true,
+		},
+		{
+			in:  "j",
+			out: nil,
+			err: true,
+		},
+	}
 
+	for _, test := range tests {
+		csv, err := LineToCSV(test.in)
+		if test.err && err != nil {
+			continue
+		}
+		if err != nil {
+			t.Errorf("Error when converting line [%s]: %v", test.in, err)
+			continue
+		}
+		for i, v := range test.out {
+			if v != csv[i] {
+				t.Errorf("Error when converting line [%s]:\n(exp)%v != (got)%v", test.in, test.out, csv)
+			}
+		}
+	}
 }

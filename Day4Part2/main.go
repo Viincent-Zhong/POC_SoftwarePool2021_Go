637bf64a535e1af612533ae3857fe67bc4140beb
Vincent Zhong <vzhong@localhost.localdomain>
2021-02-18 19:29:44 +0100

    [FIX] Exo 1 cookie find, also didnt do the exo 2 (i was wrong)

--- Day4Part2/main.go
+++ Day4Part2/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"SoftwareGoDay4/routes"
-	"SoftwareGoDay4/user"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	user.Index = 0
 	routes.ApplyRoute(router)
 	router.Run(":8080")
 }
--- Day4Part2/routes/DealRoutes.go
+++ Day4Part2/routes/DealRoutes.go
@@ -4,6 +4,7 @@ import (
 	"SoftwareGoDay4/softcrypto"
 	"SoftwareGoDay4/user"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,6 +19,7 @@ func handle_body(c *gin.Context) {
 	user.UserDB = append(user.UserDB, tmp...)
 	fmt.Printf("%#v\n", tmp)
 	c.JSON(http.StatusOK, tmp)
+	c.String(http.StatusOK, "\nuser created succesfully")
 	//fmt.Println(user.UserDB[0])
 }
 
@@ -27,26 +29,32 @@ func handle_signin(c *gin.Context) {
 	if err != nil {
 		return
 	}
-	for _, each := range user.UserDB {
+	for i, each := range user.UserDB {
 		if tmp == each {
 			tmp.Email = softcrypto.Encrypt(tmp.Email, user.APISECRET)
 			tmp.Password = softcrypto.Encrypt(tmp.Password, user.APISECRET)
-			c.Writer.Header().Add("Cookie", tmp.Email)
-			each = tmp
-			break
+			c.SetCookie("Cookie", tmp.Email, 10000, "/", "locasthost", false, true)
+			user.UserDB[i] = tmp
+			c.String(http.StatusOK, "\nFOUND")
+			return
 		}
 	}
+	c.String(http.StatusBadRequest, "Couldn't find the user")
 }
 
 func handle_cookie(c *gin.Context) {
 	str, err := c.Cookie("Cookie")
 	if err == http.ErrNoCookie {
+		c.String(http.StatusBadRequest, "Can't find the cookie")
+		log.Fatal("non")
 		return
 	}
 	for _, each := range user.UserDB {
 		if str == each.Email {
-			fmt.Printf(softcrypto.Decrypt(each.Email, user.APISECRET))
-			fmt.Printf(softcrypto.Decrypt(each.Password, user.APISECRET))
+			c.String(http.StatusOK, "Found cookie\n")
+			c.String(http.StatusOK, softcrypto.Decrypt(each.Email, user.APISECRET))
+			c.String(http.StatusOK, "\n")
+			c.String(http.StatusOK, softcrypto.Decrypt(each.Password, user.APISECRET))
 		}
 	}
 }
